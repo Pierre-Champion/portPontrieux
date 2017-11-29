c118fd075431add312daa4beddcd9a239394b03f
julienv56 <>
2017-11-29 09:46:25 +0100

    Mon premier dépot avec tous les fichiers

---
+++ src/portpontrieuxBundle/Controller/DefaultController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace portpontrieuxBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('portpontrieuxBundle:Default:index.html.twig');
+    }
+
+    
+}
